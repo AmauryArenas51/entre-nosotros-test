7bfac488c5e8afc718da3fe6e581de6e58694867
Amaury Arenas <aarenas@fad.unam.mx>
2022-11-15 01:59:26 -0600

    Interactividad de las pistas básica

--- landingpage/script.js
+++ landingpage/script.js
@@ -20,3 +20,45 @@
 })(document);
 
 /* ********* Drag&Drop ********* */
+
+const CLUE = 3;
+
+let draggableElement = document.querySelector(".draggable-elements");
+let droppableElement = document.querySelector(".droppable-elements");
+
+let clues = document.querySelectorAll(".draggable-image");
+clues = [...clues];
+clues.forEach((clues) => {
+  clues.addEventListener("dragstart", (event) => {
+    event.dataTransfer.setData("text", event.target.id);
+  });
+});
+
+let clueNames = document.querySelectorAll(".clue-names");
+let wrongMsg = document.querySelector(".wrong");
+let points = 0;
+clueNames = [...clueNames];
+clueNames.forEach((clueNames) => {
+  clueNames.addEventListener("dragover", (event) => {
+    event.preventDefault();
+  });
+  clueNames.addEventListener("drop", (event) => {
+    const draggableElementData = event.dataTransfer.getData("text");
+    let clueElement = document.querySelector(`#${draggableElementData}`);
+
+    if (event.target.innerText == draggableElementData) {
+      console.log("si");
+      event.target.innerHTML = "";
+      points++;
+      event.target.appendChild(clueElement);
+      wrongMsg.innerText = "";
+
+      if (points == CLUE) {
+        draggableElement.innerHTML = `<p class="win">Ganaste</p>`;
+      }
+    } else {
+      console.log("no");
+      wrongMsg.innerText = "Ups";
+    }
+  });
+});
