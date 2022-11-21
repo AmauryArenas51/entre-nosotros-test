70e63f34fd2316621da5e2801737fc3625f95a76
Amaury Arenas <aarenas@fad.unam.mx>
2022-11-21 03:37:46 -0600

    Quiz con inputs tipo radio funcionando

--- landingpage/script.js
+++ landingpage/script.js
@@ -21,121 +21,103 @@
 
 /* ********* Details Clues ********* */
 
+/* class Clues {
+  constructor() {
+    this.canPlay = false;
+  }
+}
 const mainImage1 = document.querySelector(".main-clue-image-1");
 const thumbnail1 = document.querySelectorAll(".clue-image-1");
 
 const mainImage2 = document.querySelector(".main-clue-image-2");
 const thumbnail2 = document.querySelectorAll(".clue-image-2");
 
+const mainImage3 = document.querySelector(".main-clue-image-3");
+const thumbnail3 = document.querySelectorAll(".clue-image-3");
+
+const mainImage4 = document.querySelector(".main-clue-image-4");
+const thumbnail4 = document.querySelectorAll(".clue-image-4");
+
 const activeClue1 = document.querySelector(".active-clue-1");
 
 const activeClue2 = document.querySelector(".active-clue-2");
 
+const activeClue3 = document.querySelector(".active-clue-3");
+
+const activeClue4 = document.querySelector(".active-clue-4");
+
 let revelation = document.querySelector(".resolution");
 
-let score = 0;
-
-/* const mainImage1 = document.querySelector(".main-clue-image-2");
-const clue1 = document.querySelectorAll(".clue1"); */
-/* const clue1 = document.querySelector("#"); */
-
-/* thumbnail1.forEach((thumb) => {
-  thumb.addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", () => {
+  function selectClue() {
     const active = document.querySelector(".active-clue-1");
     active.classList.remove("active-clue-1");
     this.classList.add("active-clue-1");
     mainImage1.src = this.src;
-    let trueClue = document.getElementsByClassName("active-clue-1")[0].id;
-    if (trueClue == 2) {
-      revelation.innerHTML = "<p>Funciona</p>";
-      score++;
-    } else {
-      revelation.innerHTML = "";
-      score = 0;
-    }
+  }
+
+  const clues = thumbnail1;
+  clues.forEach((clue) => {
+    clue.addEventListener("click", selectClue);
   });
-});
-
-thumbnail2.forEach((thumb) => {
-  thumb.addEventListener("click", function () {
-    const active = document.querySelector(".active-clue-2");
-    active.classList.remove("active-clue-2");
-    this.classList.add("active-clue-2");
-    mainImage2.src = this.src;
-  });
-});
-
-if (score == 1) {
-  revelation.innerHTML = "<p>Funciona</p>";
-} else {
-  revelation.innerHTML = "";
-}
-
-console.log(score); */
-
-thumbnail1.forEach((thumb) => {
-  thumb.addEventListener("click", function () {
-    const active = document.querySelector(".active-clue-1");
-    active.classList.remove("active-clue-1");
-    this.classList.add("active-clue-1");
-    mainImage1.src = this.src;
-    let trueClue = document.getElementsByClassName("active-clue-1")[0].id;
-    if (trueClue == 2) {
-      /* revelation.innerHTML = "<p>Funciona</p>"; */
-      score++;
-    } else {
-      /* revelation.innerHTML = ""; */
-      score = 0;
-    }
-    if (score == 2) {
-      revelation.innerHTML = "<p>Funciona</p>";
-    } else {
-      revelation.innerHTML = "";
-    }
-  });
-});
-
-thumbnail2.forEach((thumb) => {
-  thumb.addEventListener("click", function () {
-    const active = document.querySelector(".active-clue-2");
-    active.classList.remove("active-clue-2");
-    this.classList.add("active-clue-2");
-    mainImage2.src = this.src;
-    let trueClue = document.getElementsByClassName("active-clue-1")[0].id;
-    if (trueClue == 2) {
-      /* revelation.innerHTML = "<p>Funciona</p>"; */
-      score++;
-    } else {
-      /* revelation.innerHTML = ""; */
-      score = 0;
-    }
-    if (score == 2) {
-      revelation.innerHTML = "<p>Funciona</p>";
-    } else {
-      revelation.innerHTML = "";
-    }
-  });
-});
-
-/* function imageSelector(thumbnail, activeClue, number, mainImage) {
-  thumbnail.forEach((thumb) => {
-    thumb.addEventListener("click", function () {
-      const active = activeClue;
-      active.classList.remove("active-clue-1");
-      this.classList.add("active-clue-1");
-      mainImage.src = this.src;
-    });
-  });
-}
-
-imageSelector(thumbnail1, activeClue1, 1, mainImage1);
-imageSelector(thumbnail2, activeClue2, 2, mainImage2); */
-
-/* clue1.forEach((clue) => {
-  clue.addEventListener("click", function () {
-    const active = document.querySelector(".active-clue");
-    active.classList.remove("active-clue");
-    this.classList.add("active-clue");
-    mainImage1.src = this.src;
-  });
+  new Clues();
 }); */
+
+/* function verificarRespuestas() {
+  let total = 1;
+  let puntos = 0;
+
+  let myForm = document.forms["quizForm"];
+  let respuestas = ["c", "c"];
+
+  for (let i = 0; i <= total; i++) {
+    if (myForm["p" + i].value === null || myForm["p" + i].value === "") {
+      alert("Por favor responde la pregunta " + i);
+      return false;
+    } else {
+      if (myForm["p" + i].value === respuestas[i - 1]) {
+        puntos++;
+      }
+    }
+  }
+
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML =
+    `<h3>Obtuviste <span>` +
+    puntos +
+    `</span> de <span>'</span>` +
+    total +
+    ` puntos</h3>`;
+
+  return false;
+}
+
+function reset(); */
+
+function check() {
+  let question1 = document.quiz.p1.value;
+  /*   let question2 = document.quiz.p2.value;
+  let question3 = document.quiz.p3.value; */
+
+  let correct = 0;
+
+  if (question1 == "a") {
+    correct++;
+  }
+  /*   if (question2 == "b") {
+    correct++;
+  }
+  if (question3 == "c") {
+    correct++;
+  } */
+
+  document.getElementById("after-submit").style.visibility = "visible";
+  document.getElementById("number-correct").innerHTML =
+    "Tienes " + correct + " opciones correctas";
+
+  console.log(correct);
+}
+
+function myFunction() {
+  location.reload();
+}
